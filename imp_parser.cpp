61679e0f80da84a618a7f47ebd94657e9f3bd55f
Diegospf12 <94090499+Diegospf12@users.noreply.github.com>
2023-11-24 21:58:41 -0500

    Update imp_parser.cpp

--- imp_parser.cpp
+++ imp_parser.cpp
@@ -88,8 +88,8 @@ Token* Scanner::nextToken() {
       c = nextChar();
       if (c == '/') {
         while (c != '\n' && c != '\0') c = nextChar();
-        if (c == '\0') return new Token(Token::END);
-        return nextToken();
+        if (c == '\0') token = new Token(Token::END);
+        token = nextToken();
       } else {
         rollBack(); 
         token = new Token(Token::DIV); 
@@ -299,7 +299,8 @@ Stm* Parser::parseStatement() {
       exit(0);
     }
     e = parseExp();
-    s = new DoWhileStatement(e,tb);
+    if (!match(Token::ENDDO)) s = new DoWhileStatement(e,tb);
+    else s = new DoWhileStatement(e,tb);
   } else if (match(Token::IF)) {
       e = parseExp();
       if (!match(Token::THEN))
